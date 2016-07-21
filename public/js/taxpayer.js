662c3b884c39bc6410342d19e9adde2c211133a0
Joram Clervius <clervius@gmail.com>
2016-07-21 17:12:57 -0400

    Lots of change

---
+++ public/js/taxpayer.js
@@ -0,0 +1,8 @@
+var form = $("#taxpayerCreate").show();
+
+form.steps({
+	headerTag: 'h3',
+	bodyTag: 'fieldset',
+	transitionEffect: 3,
+	cssClass: 'wizard'
+});
--- server/config/routes.js
+++ server/config/routes.js
@@ -3,4 +3,12 @@
 var path = require('path');
 var express = require('express');
 
-module.exports = function(app, passport){};
+module.exports = function(app, passport){
+
+	app.get('/form', function(req, res){
+		res.render('form')
+	});
+	app.get('*', function(req, res){
+		res.render('index');
+	});
+};
